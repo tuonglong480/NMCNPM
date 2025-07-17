984a470a21516d5517965e015f5725d76b53e9e6
tuonglong480 <long.2274802010499@vanlanguni.vn>
2025-07-18 00:19:53 +0700

    Code đã Refactor theo nguyên tắc SOLID/KISS/YAGNI

--- Source.java
+++ Source.java
@@ -1,3 +1,18 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 // Giả định có các lớp/phương thức riêng biệt cho TaskDataAccess, TaskValidator, v.v.
 
 public class PersonalTaskManagerRefactored {
